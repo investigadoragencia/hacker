d117b2189788e6d13eeec6c278d1a78d914024da
investigadoragencia <investigadoragencia@gmail.com>
2025-11-07 13:57:54 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
+const Home = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao Hacker Case</h1>
+      <p>Escolha uma etapa no menu acima.</p>
+    </div>
+  )
+}
+
+export default Home
+import React from 'react'
+
 export default function Home() {
   return (
     <div>
