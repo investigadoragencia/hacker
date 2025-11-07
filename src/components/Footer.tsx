b69c04be38cf912a44f3dd1eb60f6ac497ce27cc
investigadoragencia <investigadoragencia@gmail.com>
2025-11-07 13:42:15 -0300

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 p-4 text-center">
+      © 2025 Agência Investigativa
+    </footer>
+  )
+}
