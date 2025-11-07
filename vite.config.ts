f078b466fee49df39b613146f362eb78acd3fd62
investigadoragencia <investigadoragencia@gmail.com>
2025-11-07 14:36:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/hacker/', // <- caminho do GitHub Pages
   plugins: [react()],
+  base: '/hacker/', // <--- muito importante para GitHub Pages
+  resolve: {
+    alias: {
+      '@': '/src',
+    }
+  }
 })
